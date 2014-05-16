222798a31b6878707051aa220fc2494aa01b644a
Zachary Adam Kaplan <razic@viralkitty.com>
2014-05-15 21:40:24 -0700

    added kit spec

---
+++ spec/kit_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'typekitty'
+
+describe "Typekitty::Kit" do
+    let :kit_name do
+        'viralkitty'
+    end
+
+    let :kit do
+        Typekitty::Kit.new kit_name
+    end
+
+    describe "#name" do
+        it "should have a name" do
+            expect(kit.name).to be
+        end
+    end
+end
