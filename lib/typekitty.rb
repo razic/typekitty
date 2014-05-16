8122c4034e5f58a95a1bd22049156f14e56de513
Zachary Adam Kaplan <razic@viralkitty.com>
2014-05-15 20:34:24 -0700

    prettier

--- lib/typekitty.rb
+++ lib/typekitty.rb
@@ -1,1 +1,6 @@
-require File.dirname(__FILE__) + '/typekit/cli'
+# Add the lib to the loadpath
+$:.unshift File.dirname(__FILE__)
+
+# Load the libraries
+require 'typekitty/kit'
+require 'typekitty/client'
