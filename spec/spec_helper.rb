439344f095ee106b082b62e974aa00dd0241f176
Zachary Adam Kaplan <razic@viralkitty.com>
2014-05-16 16:22:22 -0700

    added vcr back

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'rspec'
+require 'vcr'
 require 'webmock/rspec'
 
 require_relative '../lib/typekitty'
+
+VCR.configure do |c|
+    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    c.hook_into :webmock
+end
--- spec/typekitty/api_spec.rb
+++ spec/typekitty/api_spec.rb
@@ -1,51 +1,28 @@
-require File.expand_path('..', __FILE__) + '/spec_helper'
+require_relative '../spec_helper'
 
 describe "Typekitty::API" do
-    let :api_url do
-        'https://typekit.com/api/v1/json'
-    end
-
-    let :api_token do
-        'foobar'
-    end
-
     let :api do
         Typekitty::API
     end
 
     before :each do
-        api.default_params :token => api_token
+        api.default_params :token => 'b7657b3f0a75e429dd5c8b6c15c5e8514548681d'
     end
 
     describe '#kits' do
-        let :kits_url do
-            api_url + '/kits' + '?token=' + api_token
-        end
-
-        let :kits_json do
-            '{"kits":[{"id":"foo","link":"bar"}, {"id":"bar", "link":"foo"}]}'
-        end
-
-        describe '200 OK' do
-            before :each do
-                stub_request(:get, kits_url).
-                    to_return(:status => 200, :body => kits_json)
-            end
-
-            it 'should get the kits' do
-                kits = api.kits
-                expect(kits).to include 'foo'
-                expect(kits).to include 'bar'
+        describe 'GET 200' do
+            it 'return kits by their `id` in an array' do
+                VCR.use_cassette 'GET_200_kits' do
+                    expect(api.kits).to include 'ayh2hmf'
+                end
             end
         end
 
         describe '401 Unauthorized' do
-            before :each do
-                stub_request(:get, kits_url).to_return(:status => 401)
-            end
-
-            it 'should return an empty array' do
-                expect(api.kits).to be_empty
+            xit 'should return an empty array' do
+                VCR.use_cassette 'GET_401_kits' do
+                    expect(api.kits).to be_empty
+                end
             end
         end
     end
